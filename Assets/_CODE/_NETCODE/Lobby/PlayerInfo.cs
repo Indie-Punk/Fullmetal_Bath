81cbf9bb40f80128a92c040a7ba82cc3d708c933
IndiePunk <rekunigato@gmail.com>
2025-02-07 21:21:50 +0300

    - add test muliplayer staff

--- Assets/_CODE/_NETCODE/Lobby/PlayerInfo.cs
+++ Assets/_CODE/_NETCODE/Lobby/PlayerInfo.cs
@@ -1,9 +1,23 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
 
 namespace _CODE._NETCODE.Lobby
 {
     public class PlayerInfo : MonoBehaviour
     {
-        
+        [SerializeField] private TMP_Text playerName;
+        public string steamName;
+        public ulong steamId;
+        public GameObject readyImage;
+        public bool isReady;
+
+        private void Start()
+        {
+            readyImage.SetActive(false);
+            playerName.text = steamName;
+        }
     }
 }
